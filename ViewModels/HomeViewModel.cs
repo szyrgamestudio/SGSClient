7114ab89204bb3c77b47f88df0895c16d79da1d4
M455YN <m455yn@local>
2024-10-31 21:39:17 +0100

    Revamped HomePage Backend

--- ViewModels/HomeViewModel.cs
+++ ViewModels/HomeViewModel.cs
@@ -1,10 +1,61 @@
 ﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Controls;
+using Microsoft.UI.Xaml.Media.Animation;
+using SGSClient.Contracts.Services;
+using SGSClient.Views;
+using System.Windows.Input;
 
-namespace SGSClient.ViewModels;
-
-public class HomeViewModel : ObservableRecipient
+namespace SGSClient.ViewModels
 {
-    public HomeViewModel()
+    public class HomeViewModel : ObservableRecipient
     {
+        #region Fields
+        private Visibility _scrollBackButtonVisibility;
+        private Visibility _scrollForwardButtonVisibility;
+        private readonly INavigationService _navigationService;
+        #endregion
+
+        #region Constructor
+        public HomeViewModel(INavigationService navigationService)
+        {
+            ScrollBackButtonVisibility = Visibility.Collapsed;
+            ScrollForwardButtonVisibility = Visibility.Collapsed;
+
+            _navigationService = navigationService;
+            NavigateToGamesCommand = new RelayCommand(NavigateToGames);
+            NavigateToLoginCommand = new RelayCommand(NavigateToLogin);
+        }
+        #endregion
+
+        #region Properties
+        public Visibility ScrollBackButtonVisibility
+        {
+            get => _scrollBackButtonVisibility;
+            set => SetProperty(ref _scrollBackButtonVisibility, value);
+        }
+
+        public Visibility ScrollForwardButtonVisibility
+        {
+            get => _scrollForwardButtonVisibility;
+            set => SetProperty(ref _scrollForwardButtonVisibility, value);
+        }
+
+        public ICommand NavigateToGamesCommand { get; }
+        public ICommand NavigateToLoginCommand { get; }
+        #endregion
+
+        #region Navigation Methods
+        private void NavigateToGames()
+        {
+            _navigationService.NavigateTo(typeof(GamesViewModel).FullName!);
+        }
+
+        private void NavigateToLogin()
+        {
+            _navigationService.NavigateTo(typeof(LoginViewModel).FullName!);
+        }
+        #endregion
     }
 }
--- Views/HomePage.xaml.cs
+++ Views/HomePage.xaml.cs
@@ -1,79 +1,61 @@
 ﻿using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Controls;
 using SGSClient.ViewModels;
-using Microsoft.UI.Xaml.Media.Animation;
 
-namespace SGSClient.Views;
-public sealed partial class HomePage : Page
+namespace SGSClient.Views
 {
-    //private readonly bool isFirstRun = true;
-    public HomeViewModel ViewModel
+    public sealed partial class HomePage : Page
     {
-        get;
-    }
+        #region Properties
+        public HomeViewModel ViewModel { get; }
+        #endregion
 
-    public HomePage()
-    {
-        ViewModel = App.GetService<HomeViewModel>();
-        InitializeComponent();
-    }
-
-
-    private void Scroller_ViewChanging(object sender, ScrollViewerViewChangingEventArgs e)
-    {
-        if (e.FinalView.HorizontalOffset < 1)
+        #region Constructor
+        public HomePage()
         {
-            ScrollBackBtn.Visibility = Visibility.Collapsed;
+            ViewModel = App.GetService<HomeViewModel>();
+            InitializeComponent();
         }
-        else if (e.FinalView.HorizontalOffset > 1)
+        #endregion
+
+        #region Event Handlers
+        private void Scroller_ViewChanging(object sender, ScrollViewerViewChangingEventArgs e)
         {
-            ScrollBackBtn.Visibility = Visibility.Visible;
+            ViewModel.ScrollBackButtonVisibility = e.FinalView.HorizontalOffset < 1 ? Visibility.Collapsed : Visibility.Visible;
+            ViewModel.ScrollForwardButtonVisibility = e.FinalView.HorizontalOffset > scroller.ScrollableWidth - 1 ? Visibility.Collapsed : Visibility.Visible;
         }
 
-        if (e.FinalView.HorizontalOffset > scroller.ScrollableWidth - 1)
+        private void Scroller_SizeChanged(object sender, SizeChangedEventArgs e)
         {
-            ScrollForwardBtn.Visibility = Visibility.Collapsed;
+            UpdateScrollButtonsVisibility();
         }
-        else if (e.FinalView.HorizontalOffset < scroller.ScrollableWidth - 1)
-        {
-            ScrollForwardBtn.Visibility = Visibility.Visible;
-        }
-    }
-    private void ScrollBackBtn_Click(object sender, RoutedEventArgs e)
-    {
-        scroller.ChangeView(scroller.HorizontalOffset - scroller.ViewportWidth, null, null);
-    }
-    private void ScrollForwardBtn_Click(object sender, RoutedEventArgs e)
-    {
-        scroller.ChangeView(scroller.HorizontalOffset + scroller.ViewportWidth, null, null);
-    }
-    private void Scroller_SizeChanged(object sender, SizeChangedEventArgs e)
-    {
-        UpdateScrollButtonsVisibility();
-    }
-    private void UpdateScrollButtonsVisibility()
-    {
-        if (scroller.ScrollableWidth > 0)
-        {
-            ScrollForwardBtn.Visibility = Visibility.Visible;
-        }
-        else
-        {
-            ScrollForwardBtn.Visibility = Visibility.Collapsed;
-        }
-    }
 
-    private void GamesButton_Click(object sender, RoutedEventArgs e)
-    {
-        Frame.Navigate(typeof(GamesPage), null, new DrillInNavigationTransitionInfo());
-    }
+        private void ScrollBackBtn_Click(object sender, RoutedEventArgs e)
+        {
+            scroller.ChangeView(scroller.HorizontalOffset - scroller.ViewportWidth, null, null);
+        }
 
-    private void UploadButton_Click(object sender, RoutedEventArgs e)
-    {
-        Frame.Navigate(typeof(UploadGamePage), null, new DrillInNavigationTransitionInfo());
-    }
-    private void LoginButton_Click(object sender, RoutedEventArgs e)
-    {
-        Frame.Navigate(typeof(LoginPage), null, new DrillInNavigationTransitionInfo());
+        private void ScrollForwardBtn_Click(object sender, RoutedEventArgs e)
+        {
+            scroller.ChangeView(scroller.HorizontalOffset + scroller.ViewportWidth, null, null);
+        }
+
+        private void GamesButton_Click(object sender, RoutedEventArgs e)
+        {
+            ViewModel.NavigateToGamesCommand.Execute(null);
+        }
+
+        private void LoginButton_Click(object sender, RoutedEventArgs e)
+        {
+            ViewModel.NavigateToLoginCommand.Execute(null);
+        }
+        #endregion
+
+        #region Private Methods
+        private void UpdateScrollButtonsVisibility()
+        {
+            ViewModel.ScrollForwardButtonVisibility = scroller.ScrollableWidth > 0 ? Visibility.Visible : Visibility.Collapsed;
+        }
+        #endregion
     }
 }
